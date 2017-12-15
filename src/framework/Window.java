83991ed47400a1d1ce66902d4ba7e8bed3ec1c43
Wesley <wesleylondonwl@gmail.com>
2017-12-14 23:49:12 -0500

    -tested zooming while also making minor changes

--- src/framework/Window.java
+++ src/framework/Window.java
@@ -21,7 +21,8 @@ public class Window extends JPanel {
 
     public int scrW = screenSize.width;
     public int scrH = screenSize.height;
-    private double time = 0;
+    private double time = 1;
+    public double zoom = 1;
 
     Cell cellA,cellB;
     ArrayList<Cell> Cells = new ArrayList<Cell>();
@@ -31,7 +32,7 @@ public class Window extends JPanel {
         timer.scheduleAtFixedRate(new Schedule(), 1000, 10);
 
         cellA = new Cell(220,220, 100);
-        cellB = new Cell(500,220, 100);
+        cellB = new Cell(220,220, 1);
         Cells.add(cellA);
         Cells.add(cellB);
         cellB.setColor(255,119,81);
@@ -44,15 +45,13 @@ public class Window extends JPanel {
 
     public void paint(Graphics g) {
         super.paint(g);
-        time+= 0.01;
+
+
         Graphics2D g2d = (Graphics2D) g;
-        double scale = -Math.abs(Math.sin(time/2)) - 1;
         System.out.println("("+cellA.x+ ", " + cellA.y+")");
-
-        //g2d.translate(20,20);
-        //g2d.scale(scale, scale);
-        //g2d.translate(-scrW/2,-scrH/2);
-
+       g2d.translate(scrW/2,scrH/2);
+       g2d.scale(zoom, zoom);
+       g2d.translate(-scrW/2,-scrH/2);
 
         for(int i = 1; i<360;i+=20){
         }
@@ -60,10 +59,12 @@ public class Window extends JPanel {
         for(int n = 0; n<Cells.size();n++){
             Cell cell = Cells.get(n);
             cell.render(g2d);
-            g2d.setColor(Color.RED);
-            g2d.fill(new Ellipse2D.Double(cell.x -5,cell.y - 5, 10, 10));
+            //g2d.setColor(Color.RED);
+            //g2d.fill(new Ellipse2D.Double(cell.x -5,cell.y - 5, 10, 10));
         }
+
         g2d.dispose();
+
     }
 
     class Schedule extends TimerTask {
